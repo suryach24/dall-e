dd7e8391a8c1291df33aeab2a965a3556fb07a42
Surya Chandra Kukunuri <sckukunuri2020@gmail.com>
2023-09-22 09:48:14 +0530

    changed generating button

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -134,7 +134,7 @@ const CreatePost = () => {
          onClick={generateImage}
          className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
         >
-          {generateImage ? 'Generating...' : 'Generate'}
+          {generateImage ? 'Generate' : 'Generating...'}
         </button>
       </div>
 
